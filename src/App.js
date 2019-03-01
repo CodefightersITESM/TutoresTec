9be9deab32650a4efdfd2c10fb921cca57825880
Hector Diaz <diazhector98@gmai.com>
2019-03-01 15:43:53 -0600

    Add firebase auth to create account

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import firebase from 'firebase/app';
+import 'firebase/auth';
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      action: "login"
+    };
+  }
+
+  handleToggleAction = () => {
+    this.setState((prevState) => {
+      console.log(prevState);
+      return {
+        action: prevState.action === "login" ? "signup" : "login"
+      }
+    })
+  }
+
+  handleSignUp = () => {
+    const provider = firebase.auth.EmailAuthProvider();
+    const auth = firebase.auth();
+    auth.createUserWithEmailAndPassword(this.state.email, this.state.password).then((r) => {
+      console.log(r);
+    });
+    console.log(provider);
+    console.log(auth);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    console.log(e.target.value);
+  }
+
+
+  getAuthComponent = () => {
+    if(this.state.action === "login"){
+      return (        
+        <div className="AuthContainer">
+          <input placeholder="Correo" name="email" className="AuthComponent" onChange={this.handleChange} value={this.state.email}/>
+          <input placeholder="Contraseña" name="password" type="password" className="AuthComponent" onChange={this.handleChange} value={this.state.password}/>
+          <button>Ingresar</button>
+          </div>
+    );
+    } else {
+      return (
+        <div className="AuthContainer">
+        <input placeholder="Correo" name="email" className="AuthComponent" onChange={this.handleChange} value={this.state.email}/>
+        <input placeholder="Contraseña" name="password" type="password" className="AuthComponent" onChange={this.handleChange} value={this.state.password}/>
+        <input placeholder="Confirmar Contraseña" type="password" className="AuthComponent"/>
+        <form>
+          <input type="radio" name="type" value="estudiante" />Estudiante
+          <input type="radio" name="type" value="tutor" />Tutor
+        </form>
+        <button onClick={this.handleSignUp}>Crear</button>
+      </div>
+      );
+    }
+  }
+
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <h1>Tutores Tec</h1>
+        <h2>{this.state.action === "login" ? "Login" : "Sign Up"}</h2>
+        {this.getAuthComponent()}
+        <button onClick={this.handleToggleAction}>{this.state.action === "login" ? "Crear Cuenta" : "Entrar a Cuenta"}</button>
+
+        </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDVfChrnv_Ltg46Ewe7bxFoJSMAoC1oqro",
+    authDomain: "tutores-tec.firebaseapp.com",
+    databaseURL: "https://tutores-tec.firebaseio.com",
+    projectId: "tutores-tec",
+    storageBucket: "tutores-tec.appspot.com",
+    messagingSenderId: "742317966501"
+  };
+
+firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
